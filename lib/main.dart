33cdaf0ca96fe3712312b13f12c3f71c9f27c483
Zedan Saheer <zed.saheer5@gmail.com>
2022-10-04 03:44:12 +0530

    updated icons for IOS and android , Learnt scaffold and many Material widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      const MaterialApp(
-        home: Center(
-          child: Text('Hello World'),
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: const Text('Gotham City'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: const Center(
+            child: Image(
+              image: AssetImage('images/city.jpg'),
+            ),
+          ),
         ),
       ),
     );
